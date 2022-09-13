e0d3049cb96a9d29eca7b0fee0758cbbdc2c4855
IngDavidTM <david-1011@hotmail.com>
2022-09-13 17:47:08 -0500

    add second page on html, css and js

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function wrapFunction() {
+window.addEventListener('DOMContentLoaded', () => {
   const wrap = document.getElementById('nav-wrapper');
-  if (wrap.style.display === 'flex') {
-    wrap.style.display = 'none';
-  } else {
-    wrap.style.display = 'flex';
+  const openWrap = document.getElementById('wrapper');
+  const closeWrap = document.getElementById('close-wrap');
+  function wrapFunction() {
+    if (wrap.style.display === 'flex') {
+      wrap.style.display = 'none';
+    } else {
+      wrap.style.display = 'flex';
+    }
   }
-}
-const openWrap = document.getElementById('wrapper');
-const closeWrap = document.getElementById('close-wrap');
-openWrap.addEventListener('click', wrapFunction);
-closeWrap.addEventListener('click', wrapFunction);
+  openWrap.addEventListener('click', wrapFunction);
+  closeWrap.addEventListener('click', wrapFunction);
+  const partner = document.getElementById('partner');
+  const moreMobile = document.getElementById('more-mobile');
+  function moreFunction() {
+    if (partner.style.display === 'flex') {
+      partner.style.display = 'none';
+      moreMobile.innerHTML = 'MORE <i class="fa-solid fa-chevron-down">';
+    } else {
+      partner.style.display = 'flex';
+      moreMobile.innerHTML = 'LESS <i class="fa-solid fa-chevron-up">';
+    }
+  }
+  moreMobile.addEventListener('click', moreFunction);
+});
